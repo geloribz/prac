172787747060bb61d56592d478a9477b6ef3a8a3
geloribz <103402008+geloribz@users.noreply.github.com>
2022-08-22 20:00:34 +0800

    Update userage.py
    Used 'user_age' directly instead of having 'years'

--- python/userage.py
+++ python/userage.py
@@ -1,5 +1,4 @@
 name = input("Please enter your name: ").capitalize()
-user_age = input("Please enter your age: ")
-years = int(user_age)
-months = years * 12
-print(f"{name}, your age, {years}, is equal to {months} months.")
+user_age = int(input("Please enter your age: "))
+months = user_age * 12
+print(f"{name}, your age, {user_age}, is equal to {months} months.")
