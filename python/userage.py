71e1754c0a809f4c09f20a5806eb5a28b7548003
geloribz <103402008+geloribz@users.noreply.github.com>
2022-08-22 19:58:31 +0800

    Create userage.py
    Asks user their name and age and converts their age into months

---
+++ python/userage.py
@@ -0,0 +1,5 @@
+name = input("Please enter your name: ").capitalize()
+user_age = input("Please enter your age: ")
+years = int(user_age)
+months = years * 12
+print(f"{name}, your age, {years}, is equal to {months} months.")
