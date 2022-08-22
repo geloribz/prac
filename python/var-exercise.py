3962cef96a0a84a08ae015ee752276de24d3dda2
geloribz <103402008+geloribz@users.noreply.github.com>
2022-08-22 19:03:48 +0800

    Create var-exercise.py

---
+++ python/var-exercise.py
@@ -0,0 +1,8 @@
+# Create two variables, var_1, and var_2, both having the same value
+var_1 = 61.9
+var_2 = 61.9
+
+# Create two variables, num_1, and num_2, that when multiplied together will have a product of 16
+num_1 = 0.5
+num_2 = 32
+print(num_1*num_2)
