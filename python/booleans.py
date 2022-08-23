05ecfd9b955e0da984215e40f502dc67c9fbd8a3
geloribz <103402008+geloribz@users.noreply.github.com>
2022-08-23 10:16:43 +0800

    Update booleans.py
    Completed the code

---
+++ python/booleans.py
@@ -0,0 +1,12 @@
+print(5 == 5)
+print(5 > 5)
+print(4 != 3)
+#Comparison operators can be ==, !=, >, <, >=, <=
+
+friends = ["Ted", "Bob"]
+enemies = ["Ted", "Bob"]
+print(friends == enemies)
+#This compares whether the elements inside them are the same
+print(friends is enemies)
+#When creating a list, a space is created by Python in its memory for it. Creating another list, a different space is created
+#This is not almost used as an operator
