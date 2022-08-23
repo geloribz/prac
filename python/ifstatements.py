da22835328d2ba85df39e04ddf8270886caf7e3b
geloribz <103402008+geloribz@users.noreply.github.com>
2022-08-23 10:40:29 +0800

    Create ifstatements.py

---
+++ python/ifstatements.py
@@ -0,0 +1,10 @@
+day_of_week = input("What day is it today? : ").capitalize()
+
+if day_of_week == "Monday":
+    print("Have a great start to your week of work!")
+elif day_of_week == "Tuesday":
+    print("It's a Tuesday!")
+else:
+    print("Keep on going!")
+
+print("This always runs.")
