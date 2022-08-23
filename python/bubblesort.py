a67ddf7235dff4609a111d8343e66859b2ca156b
geloribz <103402008+geloribz@users.noreply.github.com>
2022-08-23 09:55:29 +0800

    Update bubblesort.py
    Removed iteration checks

--- python/bubblesort.py
+++ python/bubblesort.py
@@ -1,16 +1,9 @@
 #Arranges from lowest to highest
 def bubble_sort(nums):
     for setup in range(len(nums)-1,0,-1):
-        print(f"Setup range now is: {setup}")
-        index_i = 0
-        swaps = 0
         for index in range(setup):
-            index_i += 1
-            print(f"We are now in element: {index_i}")
             if nums[index] > nums[index+1]:
                 #changing the comparison operator here to '<' from '>' will arrange it from highest to lowest
-                swaps += 1
-                print(f"Swaps: {swaps}")
                 temp_nums = nums[index]
                 nums[index] = nums[index+1]
                 nums[index+1] = temp_nums
