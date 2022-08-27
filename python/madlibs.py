11089f70adb6fd232ff6f19ab24b327a240a15e5
geloribz <103402008+geloribz@users.noreply.github.com>
2022-08-27 08:47:57 +0800

    Update madlibs.py
    Updated so the output would be emphasized

--- python/madlibs.py
+++ python/madlibs.py
@@ -13,7 +13,8 @@ noun7 = input("Noun (plural): ")
 verb4 = input("Verb (ending in -ing): ")
 noun8 = input("Noun (preferably a surface): ")
 new_line = "\n"
-madlib = f'\ It was a {adj1}, cold November day. I {new_line}\
+madlib = f'{new_line}\
+    It was a {adj1}, cold November day. I {new_line}\
     woke up to the {adj2} smell of {noun1}{new_line}\
     roasting in the {noun2} downstairs. I{new_line}\
     {verb1} down the stairs to see if I could{new_line}\
@@ -22,5 +23,6 @@ madlib = f'\ It was a {adj1}, cold November day. I {new_line}\
     carried a tray of glasses full of {noun5} into{new_line}\
     the {verb3} room. When I got there, I{new_line}\
     could not believe my {noun6}! There were{new_line}\
-    {noun7} {verb4} on the {noun8}!'
+    {noun7} {verb4} on the {noun8}!{new_line}\
+        '
 print(madlib)
