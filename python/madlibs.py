b93ac9617d192544de5681b9ec69a62c67962ceb
geloribz <103402008+geloribz@users.noreply.github.com>
2022-08-27 08:43:53 +0800

    Create madlibs.py

---
+++ python/madlibs.py
@@ -0,0 +1,26 @@
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+noun1 = input("Type of bird: ")
+noun2 = input("Room in a house: ")
+verb1 = input("Verb (past tense): ")
+verb2 = input("Verb: ")
+noun3 = input("Relative's name: ")
+noun4 = input("Noun (preferably food): ")
+noun5 = input("A liquid: ")
+verb3 = input("Verb (ending in -ing): ")
+noun6 = input("Part of the body (plural): ")
+noun7 = input("Noun (plural): ")
+verb4 = input("Verb (ending in -ing): ")
+noun8 = input("Noun (preferably a surface): ")
+new_line = "\n"
+madlib = f'\ It was a {adj1}, cold November day. I {new_line}\
+    woke up to the {adj2} smell of {noun1}{new_line}\
+    roasting in the {noun2} downstairs. I{new_line}\
+    {verb1} down the stairs to see if I could{new_line}\
+    help {verb2} the dinner. My mom said,{new_line}\
+    "See if {noun3} needs a fresh {noun4}." So I{new_line}\
+    carried a tray of glasses full of {noun5} into{new_line}\
+    the {verb3} room. When I got there, I{new_line}\
+    could not believe my {noun6}! There were{new_line}\
+    {noun7} {verb4} on the {noun8}!'
+print(madlib)
